e0e9df02d31268ffa84bacd47b742af502be35a8
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-02-02 03:05:21 +0100

    install-packages.sh

---
+++ install-packages.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+print s 'Installing packages...'
+
+print s 'ZSH'
+yay -S zsh zsh-autosuggestions zsh-syntax-highlighting autojump
+
+print s 'Xorg, Intel and Amd GPU drivers'
+yay -S xorg xorg-xinit mesa lib32-mesa xf86-video-amdgpu vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vulkan-intel intel-media-driver lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau libva-utils
+
+print s 'Old intel drivers (only if necessary'
+yay -S xf86-video-intel libva-intel-driver
+
+print s 'Pulseaudio'
+yay -S pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-bluetooth
+
+print s 'Better CLI tools'
+yay -S neofetch htop bashtop micro calc autocutsel
+
+print s 'WM'
+yay -S bspwm sxhkd rofi picom polybar dunst wmrc flameshot
+
+print s 'Display manager & Polkit'
+yay -S lightdm lightdm-slick-greeter polkit-gnome
+
+print s 'Fonts, icons & themes'
+yay -S noto-fonts noto-fonts-emoji papirus-icon-theme qogir-icon-theme materia-gtk-theme lxappearance
+
+print s 'Standard GUI apps'
+yay -S alacritty xterm thunar firefox chromium nitrogen xfce4-clipman-plugin xfce4-power-manager sublime-text-3 code imv mpv vlc
+
+print s 'Pulseaudio apps'
+yay -S pavucontrol pnmixer mictray nm-applet
+
+print s 'Wine and dependencies'
+yay -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader dxvk-bin libstrangle
+
+print s 'Gaming & Social'
+yay -S steam discord lutris dino 
+
+print s 'Gamemoded*'
+yay -S meson dbus
+
+print s 'Proton-GE'
+cd ~
+wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
+sudo chmod +x cproton.sh
+./cproton.sh
