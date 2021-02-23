2d7302caf72c2d12b7a84abf23a43c9c7fd16795
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-02-23 15:48:21 +0100

    Enable/Disable scripts for monitors

--- scripts/enablescreen.sh
+++ scripts/enablescreen.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-xrandr --output HDMI-A-2 --auto --above DisplayPort-0 --mode 1920x1080 --rate 75
 xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144
+xrandr --output HDMI-A-2 --auto --above DisplayPort-0 --mode 1920x1080 --rate 75
+if wmrc reload; then
+    "$WMRC_MODULES/ui/notify" info "wmrc" "Desktop configuration reloaded successfully.";
+else
+    $WMRC_MODULES/ui/notify" warn "wmrc" "An error occurred while reloading desktop configuration.\n$ERR";
+fi
