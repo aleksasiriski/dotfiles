40296cf4dccc05a0db7b25a2bddb3cca24a0665e
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-06-08 01:49:48 +0200

    pkgs

--- install-packages.sh
+++ install-packages.sh
@@ -29,7 +29,7 @@ clear
 echo 'Intel and Amd GPU drivers'
 paru -S mesa lib32-mesa libva-utils --noconfirm
 paru -S vulkan-intel intel-media-driver libva-intel-driver --noconfirm
-paru -S xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+paru -S xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau --noconfirm
 clear
 
 echo 'Xorg'
@@ -37,7 +37,7 @@ paru -S xorg xorg-xinit
 clear
 
 echo 'Wayland Window Manager - Sway'
-paru -S sway xorg-xwayland waybar libappindicator-gtk3 wofi mako i3status qt5-wayland glfw-wayland grimshot wl-clipboard clipman firefox
+paru -S sway xorg-xwayland waybar libappindicator-gtk3 wofi mako i3status qt5-wayland glfw-wayland grimshot wl-clipboard clipman
 clear
 
 echo 'Minimal gnome'
@@ -65,7 +65,7 @@ paru -S gsfonts noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk ttf-
 clear
 
 echo 'Standard GUI apps'
-paru -S polkit-gnome alacritty devour pcmanfm-gtk3 gvfs gvfs-mtp ntfs-3g xarchiver sublime-text-4 vscodium-bin imv mpv vlc cpu-x
+paru -S polkit-gnome alacritty devour firefox pcmanfm-gtk3 gvfs gvfs-mtp xarchiver sublime-text-4 vscodium-bin imv mpv vlc cpu-x
 clear
 
 echo 'Systray apps'
@@ -73,11 +73,11 @@ paru -S pavucontrol mictray nm-connection-editor network-manager-applet
 clear
 
 echo 'Wine and dependencies'
-paru -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader dxvk-bin
+paru -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 clear
 
 echo 'Gaming'
-paru -S steam steam-tweaks proton-ge-custom-bin lutris libstrangle mangohud goverlay gamemode lib32-gamemode
+paru -S steam steam-tweaks proton-ge-custom-bin lutris dxvk-bin libstrangle mangohud goverlay gamemode lib32-gamemode
 systemctl --user enable gamemoded
 clear
 
